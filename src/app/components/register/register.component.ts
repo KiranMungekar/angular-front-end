6cad8e13f22ceff4e92505b7525b7d3a3d67388a
unknown <mungekar.kiran30@gmail.com>
2020-11-02 10:22:06 +0530

    Register user added

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,16 +11,35 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,
+              private authService:AuthService) { }
 
   registerForm:FormGroup;
 
   ngOnInit(): void {
-    this.fb.group({
+    this.registerForm=this.fb.group({
       name:new FormControl('',[Validators.required, Validators.minLength(3)]),
       email:new FormControl('',[Validators.required, Validators.email]),
-      password: new FormControl('',[Validators.required,Validators.minLength(6),Validators.minLength(20)])
+      password: new FormControl('',[Validators.required])
+      
     });
+
+    //Validators.minLength(6),Validators.minLength(20)
+  }
+
+
+  registerUser(){
+    console.log(this.registerForm.value);
+    this.authService.register(this.registerForm.value).subscribe((response:HttpResponse<any>)=>{
+      console.log(response);
+      if(response.body != null && !response.body['err']){
+        console.log(response.body['token']);
+        const token=response.body['token'];
+        localStorage.setItem('token',token)
+      }else{
+        alert('Registration failed. Please try again');
+      }
+    })
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,15 @@ export class AuthService {
 
   getGoogleAuth(){
     return this.http.get(this.uri+'api/auth/google');
+  }
+
+  register(authData):Observable<HttpResponse<any>>{
+    const data={
+      name:authData.name,
+      email:authData.email,
+      password:authData.password
+    
+    }
+    return this.http.post(this.uri+'api/auth/register/',data, {observe:'response'})
   }
 }
